5bed3b78e937cdaa68bed0f6552c2bdcc342a49e
Florina Toldea <toldea.florina1712@yahoo.com>
2021-05-26 21:04:21 +0200

    Some improvements

--- src/main/java/tierTwo/controllers/UserController.java
+++ src/main/java/tierTwo/controllers/UserController.java
@@ -1,5 +1,6 @@
 package tierTwo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tierTwo.Services.user.UserService;
@@ -8,14 +9,13 @@ import tierTwo.models.user.User;
 @RestController
 public class UserController
 {
-
+ @Autowired UserService userService;
 
    @GetMapping("/login")
   public User validateUser(User user){
 
      //call tier3 and get the response
-
-
+   return userService.validateUser(user);
 
    }
 
