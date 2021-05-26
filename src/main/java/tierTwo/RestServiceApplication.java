107d10efd5863ba839a1d6771108222fbfaa1edc
Dorin_Chira <kiradorin94@gmail.com>
2021-05-26 05:05:38 +0200

    Controller

--- src/main/java/tierTwo/RestServiceApplication.java
+++ src/main/java/tierTwo/RestServiceApplication.java
@@ -5,19 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tierTwo.networking.communication.ClientConnection;
 import tierTwo.networking.communication.SocketClient;
 
-import java.io.IOException;
-import java.rmi.NotBoundException;
-import java.util.logging.LogRecord;
-
 @SpringBootApplication
 public class RestServiceApplication {
 
 
 	public static void main(String[] args)
 	{
+
+    SpringApplication.run(RestServiceApplication.class, args);
     SocketClient client = new ClientConnection();
     client.startClient();
-    SpringApplication.run(RestServiceApplication.class, args);
 	}
 
 }
--- src/main/java/tierTwo/controllers/UserController.java
+++ src/main/java/tierTwo/controllers/UserController.java
@@ -1,14 +1,28 @@
 package tierTwo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tierTwo.models.user.User;
 
 @RestController
-@RequestMapping("/user")
-
 public class UserController
 {
    @Autowired
   UserController userController;
+
+
+   @GetMapping("/login")
+  public User validateUser(User user){
+
+     //call tier3 and get the response
+
+     User user1 = new User();
+     user1.setUserName("admin");
+     user1.setUserName("admin");
+     user1.setType("admin");
+
+     return user1;
+   }
 
 }
