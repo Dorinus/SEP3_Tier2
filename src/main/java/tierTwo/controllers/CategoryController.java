8d53e38674caf9d1abaea7255e0809688e7c2542
Dorin_Chira <kiradorin94@gmail.com>
2021-05-28 01:27:58 +0200

    Hohohohoh

---
+++ src/main/java/tierTwo/controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package tierTwo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import tierTwo.Services.category.ICategoryService;
+import tierTwo.models.category.Category;
+import tierTwo.models.user.User;
+
+import java.util.List;
+
+@RestController
+public class CategoryController
+{
+  @Autowired ICategoryService categoryService;
+
+  @GetMapping("/category")
+  public List<Category> getCategories(){
+    System.out.println("Asking for all categories");
+    return categoryService.getCategories();
+  }
+}
